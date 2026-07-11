242a1d84d5e2e86b7bb4befcc56af31b33edca76
efan <efan@efans-MacBook-Air.local>
2026-07-11 13:37:36 +0300

    Fix formatting for tasks 6 and 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str1 = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(str1[39:66] + str1[107:112] + str1[:6])
- 
+    
