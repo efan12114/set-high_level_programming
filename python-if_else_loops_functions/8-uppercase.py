5a31dc3dec9125b994e29a1eb39c3604630b3dd2
efan <efan@efans-MacBook-Air.local>
2026-07-11 16:02:30 +0300

    Add Task 8: Function to print string in uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        ascii_val = ord(char)
+        if ascii_val >= 97 and ascii_val <= 122:
+            char = chr(ascii_val - 32)
+        print("{}".format(char), end="")
+    print("")
