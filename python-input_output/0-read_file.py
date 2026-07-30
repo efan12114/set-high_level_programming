039c3f6faf95abe92fefd7b725c708327ca595bc
efan12114 <efanaddis1@gmial.com>
2026-07-30 16:52:20 +0300

    Fix empty files for README and 0-read_file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for reading a text file and printing it to stdout."""
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
