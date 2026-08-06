1c4b9319b13f97677217ba55a38771e8f44c5dc1
efan12114 <efanaddis1@gmial.com>
2026-08-06 15:47:41 +0300

    Add Task 0: Read file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Module for reading a text file and printing it to stdout."""
+"""Module for reading a text file."""
+
 
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
