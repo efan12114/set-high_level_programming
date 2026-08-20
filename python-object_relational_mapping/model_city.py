cdfd21bfd32bff06e106fb85b1808533d384d3fa
efan12114 <efanaddis1@gmial.com>
2026-08-20 18:18:57 +0300

    Task 14: Cities in state

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Contains the class definition of a City
+"""
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    """Represents a city for a MySQL database."""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
